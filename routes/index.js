b04dc1f164558a98b06eb985aa93537a6be1d53f
J <j@Js.local>
2018-09-10 22:13:45 +0800

    INIT

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const data = {};
+  data.lgUserName = 'Jia Liu';
+  data.lgUserRole = 'GMP'
+  res.render('index', data);
 });
 
 module.exports = router;
