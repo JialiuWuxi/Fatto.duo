2770330172ee5e78a173959e9e0969e8b39b9ca9
JialiuWuxi <37652140+JialiuWuxi@users.noreply.github.com>
2018-09-11 12:07:48 +0800

    Add Login page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var authHelper = require('../helpers/auth');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const data = {};
-  data.lgUserName = 'Jia Liu';
-  data.lgUserRole = 'GMP'
-  res.render('index', data);
+  res.render('login');
 });
+
+
+router.get('/auth', function(req, res, next) {
+  const parms = {};
+  parms.signInUrl = authHelper.getAuthUrl();
+  res.redirect(parms.signInUrl);
+});
+
 
 module.exports = router;
